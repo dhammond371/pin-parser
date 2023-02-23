10eee15ea863c5fc9989398346e93ede2bcd19ac
dhammond371 <dhammond371@gmail.com>
2023-02-23 22:27:26 +0000

    chore: add PinParserResult interface and skeleton ParsePin funtion

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+export interface PinParserResult {
+    success: boolean,
+    lat?: string,
+    lng?: string,
+}
+
+export const ParsePin = (pin: string): PinParserResult => {
+    if (pin == null) return { success: false };
+    
+    // handle various formats here
+    return { success: true };
+}
