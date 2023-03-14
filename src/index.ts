83d9d1b8e10e2d4db6771f466419f1492ca272b9
dhammond371 <dhammond371@gmail.com>
2023-03-14 23:07:54 +0000

    feat: handle google maps and apple maps links

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,55 @@
+import { tall } from 'tall';
 export interface PinParserResult {
     success: boolean,
     lat?: string,
     lng?: string,
 }
 
-export const ParsePin = (pin: string): PinParserResult => {
+export async function ParsePin(pin: string): Promise<PinParserResult> {
     if (pin == null) return { success: false };
+
+    // Handle the form: '12.3456,78.91011' or '(12.3456,78.91011)'
+    const splitByComma = pin.replace(/\s/g, '').split(',').map(a => a.replace('(', '').replace(')', ''));
+    if (
+        !Number.isNaN(+splitByComma?.[0]) &&
+        !Number.isNaN(+splitByComma?.[1]) &&
+        splitByComma?.[0] != null &&
+        splitByComma?.[1] != null
+        ) {
+        return {
+            success: true,
+            lat: splitByComma[0],
+            lng: splitByComma[1],
+        }
+    }
+
+    // handle google maps link: https://maps.app.goo.gl/c3rjbowSs23hQnhR8?g_st=ic
+    // handle apple maps link: https://maps.apple.com/?ll=54.745391,-110.146062&q=Dropped%20Pin&t=m
+    if (pin.slice(0, 4) === 'http') {
+        return tall(pin)
+            .then(unshortenedUrl => {
+                const latLng = unshortenedUrl.match(/-?\d+(?:\.\d+)?,\s*-?\d+(?:\.\d+)?/g)?.[0];
+                const split = latLng?.split(',');
+                const lat = split?.[0];
+                const lng = split?.[1];
+                if (!!lat && !!lng) {
+                    return {
+                        success: true,
+                        lat,
+                        lng,
+                    }
+                }
+                return {
+                    success: false,
+                }
+               
+            })
+            .catch(() => {
+                return { success: false }
+            });
+    }
+    
     
     // handle various formats here
-    return { success: true };
+    return { success: false };
 }
---
+++ src/tests.spec.ts
@@ -0,0 +1,34 @@
+import { ParsePin } from ".";
+
+describe('PinParser', () => {
+    
+    it('should return correct result when null is passed in', async () => {
+        await expect(ParsePin((null as unknown) as string)).resolves.toEqual({ success: false });
+        await expect(ParsePin((undefined as unknown) as string)).resolves.toEqual({ success: false });
+    });
+
+    it('should return correct result when lat,lng or (lat,lng) or ( lat, lng ) passed in', async () => {
+        await expect(ParsePin('123.456,78.910')).resolves.toEqual({ success: true, lat: '123.456', lng: '78.910' });
+        await expect(ParsePin('54.2391739,-111.89087521')).resolves.toEqual({ success: true, lat: '54.2391739', lng: '-111.89087521' });
+        await expect(ParsePin('(123.456,78.910)')).resolves.toEqual({ success: true, lat: '123.456', lng: '78.910' });
+        await expect(ParsePin('(54.2391739,-111.89087521)')).resolves.toEqual({ success: true, lat: '54.2391739', lng: '-111.89087521' });
+        await expect(ParsePin('( 123.456, 78.910 )')).resolves.toEqual({ success: true, lat: '123.456', lng: '78.910' });
+        await expect(ParsePin('( 54.2391739, -111.89087521 )')).resolves.toEqual({ success: true, lat: '54.2391739', lng: '-111.89087521' });
+    });
+
+   it('should return correct result when shortened google maps link is supplied', async () => {
+        await expect(ParsePin('https://maps.app.goo.gl/c3rjbowSs23hQnhR8?g_st=ic')).resolves.toEqual({
+            success: true,
+            lat: '54.8348920',
+            lng: '-113.4811306',
+        });
+   });
+
+   it('should return correct result when apple maps link is supplied', async () => {
+    await expect(ParsePin('https://maps.apple.com/?ll=54.735301,-113.446062&q=Dropped%20Pin&t=m')).resolves.toEqual({
+        success: true,
+        lat: '54.735301',
+        lng: '-113.446062',
+    });
+});
+})
